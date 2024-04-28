afd3c8ce8a12dc5f740f7edc666e6295cdc5447c
Shivu031 <128800786+Shivu031@users.noreply.github.com>
2024-04-28 11:37:18 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const navbar = ({ filterItem, menuList }) => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          {menuList.map((curElem,id) => {
+            return (
+              <button
+                className="btn-group__item" key={id}
+                onClick={() => filterItem(curElem)}>
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default navbar;
